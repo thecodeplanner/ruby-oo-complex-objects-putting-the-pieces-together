2f3fd1a0e7c27f0f87a5e2cf5159f4666731d960
thecodeplanner <thecodeplanner@gmail.com>
2020-10-27 11:34:33 -0400

    finished

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
+# Created a class of book with given properties of title, author, page count and genre. Also gave it the behavior to flip the pages!
+
+class Book
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author = author 
+    end
+
+    def author
+        @author
+    end
+
+    def page_count=(page_count)
+        @page_count = page_count
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
